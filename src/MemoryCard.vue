45a2dbfcc608c6e3ad32b6bafaa78c6208986d25
Velazquez Villegas David Alejandro <David@XMY2155-C02VW2S7G8WN.local>
2022-01-07 11:11:41 -0600

    it was changed the way to import styles

--- src/MemoryCard.vue
+++ src/MemoryCard.vue
@@ -4,6 +4,4 @@ div.memory-card(:data-selected='isSelected' @click='flipCard($event)')
    div.memory-card-image.memory-card-back(:style="{backgroundImage: 'url('+card.backImage+')'}")
 </template>
 <script src='./MemoryCard.js'></script>
-<style lang="scss" scoped>
-   @import './MemoryCard.scss';
-</style>
+<style src='./MemoryCard.scss' lang="scss" scoped></style>
